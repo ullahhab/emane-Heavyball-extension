51ef2eee509d7f5cfaa33f52cd68741ac704ffb5
ullahhab <61397521+ullahhab@users.noreply.github.com>
2020-02-24 11:33:28 -0600

    Update and rename heavyBall.cc to heavyBallqueuemanager.h

---
+++ src/heavyBallqueuemanager.h
@@ -0,0 +1,53 @@
+//TODO: HeavyBall algorithm
+#ifndef EMANEMODELSTDMABASICQUEUEMANAGERHB_HEADER_
+#define EMANEMODELSTDMABASICQUEUEMANAGERHB_HEADER_
+#include "emane/models/tdma/queuemanager.h"
+
+
+
+
+namespace EMANE
+{
+  namespace Models
+  {
+    namespace HeavyBallTDMA
+    {
+      class BasicQueueManagerHeavyBall : public QueueManager
+      {
+      public:
+         BasicQueueManagerHeavyBall(NEMId id,
+                          PlatformServiceProvider * pPlatformServiceProvider);
+
+        ~BasicQueueManagerHeavyBall();
+
+        void initialize(Registrar & registrar) override;
+
+        void configure(const ConfigurationUpdate & update) override;
+
+        void start() override;
+
+        void postStart() override;
+
+        void stop() override;
+
+        void destroy() throw() override;
+
+        size_t enqueue(std::uint8_t u8QueueIndex, DownstreamPacket && pkt) override;
+
+        std::tuple<EMANE::Models::TDMA::MessageComponents,
+                   size_t>
+          dequeue(std::uint8_t u8QueueIndex,
+                  size_t length,
+                  NEMId destination) override;
+
+        QueueInfos getPacketQueueInfo() const override;
+
+        std::map<std::uint64_t,size_t> getDestQueueLength(int priority) override;
+
+      private:
+        class Implementation;
+        std::unique_ptr<Implementation> pImpl_;
+      };
+    }
+  }
+}
